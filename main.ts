574551a1440f233571c682d2f430cc1d7076f9d1
Gregor Lohaus <gregor.lohaus@8mylez.com>
2023-07-17 22:51:16 +0200

    mvp

--- main.ts
+++ main.ts
@@ -1,109 +1,73 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice, FileSystemAdapter, TAbstractFile, PluginSettingTab,Setting,App } from 'obsidian';
+import { ChildProcess, SpawnOptionsWithoutStdio } from 'child_process';
+import { spawn } from 'child_process';
 
 // Remember to rename these classes and interfaces!
 
+
 interface MyPluginSettings {
-	mySetting: string;
+	branch: string;
+	remote: string
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	branch: 'main',
+	remote: 'origin'
 }
-
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
-
+	basepath: string;
+	loadtime: Date;
+	lastpush: Date;
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.loadtime = new Date();
+		this.lastpush = new Date();
+		let adapter = this.app.vault.adapter
+		if (adapter instanceof FileSystemAdapter) {
+			this.basepath = adapter.getBasePath()
+		}
+		this.registerEvent(this.app.vault.on("modify",this.gitPush.bind(this)))
+		this.registerEvent(this.app.vault.on("create",this.gitPush.bind(this)))
+		this.registerEvent(this.app.vault.on("delete",this.gitPush.bind(this)))
+		this.registerEvent(this.app.vault.on("rename",this.gitPush.bind(this)))
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	async gitPush(file:TAbstractFile) {
+		let adapter = file.vault.adapter
+		let d = new Date()
+		if (adapter instanceof FileSystemAdapter && (d.getTime() - this.loadtime.getTime())/1000 > 60 && (d.getTime() - this.lastpush.getTime())/1000 > 60) {
+			this.spawnProcess("git",["add","."])
+			this.spawnProcess("git",["commit","-m",`${file.name}@${d.toDateString().replace(" ","-")}`])
+			this.spawnProcess("git",["push",this.settings.remote,this.settings.branch])
+			this.lastpush = new Date();
+		}
 	}
-
+	async spawnProcess(command:string,args?: readonly string[] | undefined) {
+		if(this.basepath) {
+			let process = spawn(command,args,{
+				cwd: this.basepath
+			})
+			process.stdout.on('data', (data) => {
+				new Notice(`stdout: ${data}`);
+			  });
+			
+			process.stderr.on('data', (data) => {
+				new Notice(`stderr: ${data}`);
+			});
+			
+			process.on('close', (code) => {
+				new Notice(`child process exited with code ${code}`);
+			});
+		}
+	}
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -121,14 +85,23 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Branch')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter name of the branch you want to push')
+				.setValue(this.plugin.settings.branch)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.branch = value;
 					await this.plugin.saveSettings();
 				}));
+		new Setting(containerEl)
+		.setName('Remote')
+		.addText(text => text
+			.setPlaceholder('Enter name of the remote you want to push to')
+			.setValue(this.plugin.settings.remote)
+			.onChange(async (value) => {
+				this.plugin.settings.remote = value;
+				await this.plugin.saveSettings();
+			}));
+		
 	}
 }
